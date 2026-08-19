dfed523e55b04c0887f42631323c9f923615b3cc
Akkas Teknoloji <yazilim@aya.com.tr>
2026-08-19 09:35:45 +0300

    hakkimizda ve referanslar url i türkceleştirildi. silinen CTA dosyasına ait importlar silindi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,7 @@ import { SectorsSection } from "@/components/sections/SectorsSection";
 import { StatsSection } from "@/components/sections/StatsSection";
 import { AnnouncementsSection } from "@/components/sections/AnnouncementsSection";
 import { ReferencesSection } from "@/components/sections/ReferencesSection";
-import { ContactCTA } from "@/components/sections/ContactCTA";
 import { ArticlesSection } from "@/components/sections/Articlessection";
-import { AboutHero } from "@/components/sections/about/AboutHero";
-import { AboutStorySection } from "@/components/sections/about/AboutStorySection";
-import { TimelineSection } from "@/components/sections/about/TimelineSection";
-import { VisionMissionSection } from "@/components/sections/about/VisionMissionSeciton";
-import { ValuesSection } from "@/components/sections/about/ValueSection";
-import { AboutStatsStrip } from "@/components/sections/about/AboutStatsStrip";
 
 export default async function HomePage() {
   const content = await getHomeContent();
