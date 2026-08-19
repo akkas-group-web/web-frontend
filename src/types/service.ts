dd19e819cd7844711024acdb5ba35be4c72206dc
Betül Gündüz <betulgunduz@aya.com.tr>
2026-08-19 11:56:37 +0300

    Hizmet detay tipi types klasörüne taşındı

---
+++ src/types/service.ts
@@ -0,0 +1,10 @@
+export interface ServiceDetail {
+  id: string;
+  category: string;
+  categoryTitle: string;
+  slug: string;
+  title: string;
+  description: string;
+  image?: string;
+  content: string[];
+}
